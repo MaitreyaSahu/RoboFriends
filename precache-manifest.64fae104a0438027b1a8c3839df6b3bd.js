d21ddc61ed07aedb655602632760495325eaa383
Maitreya <mtsahu1995@gmail.com>
2020-03-26 14:48:41 +0530

    Updates

---
+++ precache-manifest.64fae104a0438027b1a8c3839df6b3bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9d71e8f51d8d3c37918bd56f323259",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01612f810ce87b38b3c3",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "75454708417009d3debb",
+    "url": "/static/css/main.303e7ac7.chunk.css"
+  },
+  {
+    "revision": "01612f810ce87b38b3c3",
+    "url": "/static/js/2.aef0a387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aef0a387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75454708417009d3debb",
+    "url": "/static/js/main.27464df4.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "67565fbce7544c4781ec3480376eb253",
+    "url": "/static/media/AndroidAssassin-WXXY.67565fbc.ttf"
+  }
+]);
